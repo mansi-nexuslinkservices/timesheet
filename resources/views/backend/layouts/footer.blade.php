5ef4313d9adcfc936f0d6b49be281f3fc5b30fd5
mansi.panchal@nexuslinkservices.in <mansi>
2023-04-13 19:36:16 +0530

    changes in footer file

--- resources/views/backend/layouts/footer.blade.php
+++ resources/views/backend/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="footer">
     <div class="copyright">
-        <p>Copyright © <a href="{{route('admin.home')}}" target="_blank">Nowonline</a> 2023</p>
+        <p>Copyright © <a href="{{route('admin.home')}}"   target="_blank">Nowonline</a> 2023</p>
     </div>
 </div>
